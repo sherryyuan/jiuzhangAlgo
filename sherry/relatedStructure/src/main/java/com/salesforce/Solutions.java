c26adac21ccd83aed84b8e35028d986a3ee28c05
sherryyuan <sherryyuan1989@gmail.com>
2018-10-18 09:12:02 -0300

    salesforce oa

---
+++ sherry/relatedStructure/src/main/java/com/salesforce/Solutions.java
@@ -0,0 +1,139 @@
+package com.salesforce;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solutions {
+
+    public boolean isPossible(int a, int b, int c, int d) {
+
+        int firstPair_min = Math.min(a, b);
+        int firstPair_max = Math.max(a, b);
+        //convert [a, b] -> [c, d]
+        // if c < min(a,b) || d < min(a,b) -> return false
+        //[3,4]->[1,5] -> false
+        if ((c < firstPair_min || d < firstPair_min)) {
+            return false;
+        }
+        if (isIntegerCouldDeCompose(c, firstPair_min, firstPair_max)) {
+            return isIntegerCouldDeCompose(d, firstPair_min, firstPair_max);
+        }
+        return false;
+    }
+
+    private boolean isIntegerCouldDeCompose(int element, int firstPair_min, int firstPair_max) {
+        while (element - firstPair_max >= 0) {
+            element -= firstPair_max;
+        }
+        while (element - firstPair_min >= 0) {
+            element -= firstPair_min;
+        }
+        if (element != 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public List<Integer> qualifyCandidateSize(int[] scores, int[] lowerBounds, int[] upperBounds) {
+
+        //edge cases
+        //lowerBounds size is not equale to upperBounds Size
+        if (!isValidInput(scores, lowerBounds, upperBounds)) {
+            return null;
+        }
+        int boundarySize = lowerBounds.length;
+        List<Integer> result = new ArrayList<>();
+
+        //sort the scores
+        Arrays.sort(scores);
+
+        for (int i = 0; i < boundarySize; i++) {
+            result.add(getQualifyCandidateNum(scores, lowerBounds[i], upperBounds[i]));
+        }
+
+        return result;
+    }
+
+    private int getQualifyCandidateNum(int[] scores, int lowerBounds, int upperBounds) {
+        // edge cases
+        if (lowerBounds >= upperBounds) {
+            return 0;
+        }
+
+        int startIndex = getFirstIndexGraterThanTarget(scores, lowerBounds);
+        //if startIndex is out of index, return 0;
+        if (startIndex >= scores.length) {
+            return 0;
+        }
+
+        int endIndex = getFirstIndexGraterThanTarget(scores, upperBounds);
+        //if endIndex is out of index, the endIndex is last index
+        if (endIndex >= scores.length) {
+            endIndex = scores.length - 1;
+        }
+        return endIndex - startIndex + 1;
+    }
+
+    private int getFirstIndexGraterThanTarget(int[] scores, int target) {
+        int left = 0;
+        int right = scores.length - 1;
+        //get first index of lowerBounds
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (scores[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        //if left(4) < target(2) -> 2|4 -> left is the first > target
+        if (scores[left] >= target) {
+            return left;
+        } else if (scores[right] == target) {
+            return right;
+        } else {
+            return right + 1;
+        }
+    }
+
+    private boolean isValidInput(int[] scores, int[] lowerBounds, int[] upperBounds) {
+        if (scores == null || lowerBounds == null || upperBounds == null) {
+            return false;
+        }
+
+        if (lowerBounds.length != upperBounds.length) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solutions solutions = new Solutions();
+        //validate method isPossible()
+        System.out.println("assert the result is true = " + solutions.isPossible(3, 2, 5, 8));
+        System.out.println("assert the result is true = " + solutions.isPossible(2, 3, 5, 8));
+        System.out.println("assert the result is false = " + solutions.isPossible(9, 2, 5, 8));
+        System.out.println("assert the result is true = " + solutions.isPossible(3, 2, 6, 8));
+        System.out.println("assert the result is false = " + solutions.isPossible(7, 5, 17, 8));
+        //validate method qualifyCandidateSize();
+        int[] scores = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] lowerBound = {1, 4};
+        int[] upperBound = {3, 8};
+
+        System.out.println("assert the result is [3, 5] " + solutions.qualifyCandidateSize(scores, lowerBound, upperBound));
+        scores[1] = 10;//[2,3,4,5,6,7,8,10]
+        upperBound[1] = 11; //[3, 11]
+        System.out.println("assert the result is [2, 6] " + solutions.qualifyCandidateSize(scores, lowerBound, upperBound));
+        //[2,3,4,5,6,7,8,10]
+        lowerBound[0] = 0;
+        System.out.println("assert the result is [2, 6] " + solutions.qualifyCandidateSize(scores, lowerBound, upperBound));
+
+        int[] scores1 = {1, 2, 3};
+        System.out.println("assert the result is 0 " + solutions.getFirstIndexGraterThanTarget(scores1, 0));
+        System.out.println("assert the result is 3 " + solutions.getFirstIndexGraterThanTarget(scores1, 4));
+        System.out.println("assert the result is 2 " + solutions.getFirstIndexGraterThanTarget(scores1, 3));
+
+    }
+}
---
+++ sherry/relatedStructure/src/main/java/com/salesforce/SolutionsII.java
@@ -0,0 +1,122 @@
+package com.salesforce;
+
+import java.util.*;
+
+public class SolutionsII {
+    // version 1: BFS
+    class Coordinate {
+        int x, y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * @param grid a boolean 2D matrix
+     * @return an integer
+     */
+    public int numIslands(boolean[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int islands = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j]) {
+                    markByBFS(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private void markByBFS(boolean[][] grid, int x, int y) {
+        // magic numbers!
+        int[] directionX = {0, 1, -1, 0};
+        int[] directionY = {1, 0, 0, -1};
+
+        Queue<Coordinate> queue = new LinkedList<>();
+
+        queue.offer(new Coordinate(x, y));
+        grid[x][y] = false;
+
+        while (!queue.isEmpty()) {
+            Coordinate coor = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                Coordinate adj = new Coordinate(
+                        coor.x + directionX[i],
+                        coor.y + directionY[i]
+                );
+                if (!inBound(adj, grid)) {
+                    continue;
+                }
+                if (grid[adj.x][adj.y]) {
+                    grid[adj.x][adj.y] = false;
+                    queue.offer(adj);
+                }
+            }
+        }
+    }
+
+    private boolean inBound(Coordinate coor, boolean[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        return coor.x >= 0 && coor.x < n && coor.y >= 0 && coor.y < m;
+    }
+
+    class Interval {
+        int start, end;
+
+        Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+    //merge interval
+
+    /**
+     * @param intervals: interval list.
+     * @return: A new interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        // write your code here
+        //edge cases
+        if (intervals == null) {
+            return null;
+        }
+        List<Interval> result = new ArrayList();
+        //sort the intervals with start index
+        intervals.sort(new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.start - o2.start;
+            }
+        });
+
+        Interval last = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval current = intervals.get(i);
+            if (last.end < current.start) {
+                result.add(last);
+                last = current;
+            } else {
+                last.end = Math.max(last.end, current.end);
+                ;
+            }
+        }
+
+        result.add(last);
+        return result;
+    }
+}
+
+
